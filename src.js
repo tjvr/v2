63bfd977b8daacb7e0f1380865ded2161a0928b9
Nathan <nathan@users.noreply.github.com>
2016-09-14 13:12:17 -0400

    Add v2.DynamicTree and v2.path.sanitize

--- src.js
+++ src.js
@@ -108,6 +108,9 @@ v2.path = {
     if (i === -1) return x
     return x.slice(i + 1)
   },
+  sanitize(x) {
+    return v2.path.resolve('', x)
+  },
   join(...parts) {
     return parts.filter(p => p).join('/')
   },
@@ -551,6 +554,125 @@ v2.Node = class Node extends v2.CyNode {
   *parents() {for (let p = this; p; p = p.parent) yield p}
 }
 
+v2.DynamicTreeItem = class DynamicTreeItem extends v2.View {
+  init() {
+    this._model = null
+    this.items = null
+    this.tree = null
+    this.isExpanded = false
+    this._content = null
+    this._childRemoved = this._childRemoved.bind(this)
+    this._childInserted = this._childInserted.bind(this)
+  }
+
+  build() {
+    return h('.v2-dynamic-tree-item',
+      this._label = h('.v2-dynamic-tree-item-label',
+        h('.v2-tree-item-disclosure')),
+      this.container = h('.v2-dynamic-tree-item-container'))
+  }
+
+  get model() {return this._model}
+  set model(value) {
+    if (this._content) this._label.removeChild(this._content)
+    if (this._model) {
+      this._model.unlisten('child inserted', this._childInserted)
+      this._model.unlisten('child removed', this._childRemoved)
+    }
+    this._model = value
+    value.on('child inserted', this._childInserted)
+    value.on('child removed', this._childRemoved)
+    if (this._label) {
+      this._label.appendChild(this._content = this.tree.template(value.data))
+      this._updateEmpty()
+    }
+    this._reload(this.tree !== this)
+  }
+
+  _updateEmpty() {
+    if (this._label) this._label.classList.toggle('v2-tree-item-empty', !this._model || this._model.children.length === 0)
+  }
+  _childRemoved(e) {
+    if (!this.items) return
+    const child = this.items[e.index]
+    if (!child) return
+    child.remove()
+    this.items.splice(e.index, 1)
+    this._updateEmpty()
+  }
+  _childInserted(e) {
+    if (!this.items) return
+    const model = this._model.children[e.index]
+    const before = this.items[e.index]
+    const item = new DynamicTreeItem({tree: this.tree, model})
+    this.items.splice(e.index, 0, item)
+    this.add(item, this.container, before.el)
+  }
+
+  toggle() {
+    if (this.isExpanded) this.collapse()
+    else this.expand()
+  }
+  expand() {
+    if (this.isExpanded) return
+    this.isExpanded = true
+    if (this._label) this._label.classList.add('v2-tree-item-expanded')
+    if (this.items) {
+      this.container.style.display = ''
+    } else {
+      this.items = []
+      for (const c of this._model.children) {
+        const item = new DynamicTreeItem({tree: this.tree, model: c})
+        this.add(item)
+        this.items.push(item)
+      }
+    }
+  }
+  collapse() {
+    if (!this.isExpanded) return
+    this.isExpanded = false
+    if (this._label) this._label.classList.remove('v2-tree-item-expanded')
+    if (this.items) {
+      this.container.style.display = 'none'
+    }
+  }
+
+  _reload(suppress) {
+    if (!this._model) return
+    const expanded = this.isExpanded
+    this.removeChildren()
+    this.items = null
+    this.isExpanded = false
+    if (expanded && !suppress) this.expand()
+  }
+}
+v2.DynamicTree = class DynamicTree extends v2.DynamicTreeItem {
+  init() {
+    super.init()
+    this.tree = this
+    this.isExpanded = true
+    this._template = content => h('.v2-tree-item-label', content)
+  }
+
+  build() {
+    return h('.v2-view.v2-dynamic-tree', {onmousedown: '_mouseDown'})
+  }
+
+  _mouseDown(e) {
+    if (h.nearest('.v2-tree-item-disclosure', e.target)) {
+      const item = h.nearest('.v2-dynamic-tree-item', e.target)
+      if (item) item.view.toggle()
+      return
+    }
+  }
+
+  get template() {return this._template}
+  set template(value) {
+    this._template = value
+    this._reload()
+  }
+}
+
 v2.Tree = class Tree extends v2.View {
   init() {
     this._model = null
@@ -580,6 +702,14 @@ v2.Tree = class Tree extends v2.View {
     this._linear.length = 0
     this._root = new v2.Tree._L(this, -1, value)
     this._root.expand()
+  }
+
+  get transform() {return this._transform}
+  set transform(value) {
+    this._transform = value
+    for (const item of this._cache.values()) {
+      if (item._visible) item._dataChanged()
+    }
   }
 
   beginAtomic() {
