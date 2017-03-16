9b4af07805e4bf30fe883f6f77bf4417e79d98bf
Nathan <nathan@users.noreply.github.com>
2017-03-16 19:11:57 -0400

    Move request to separate file

---
+++ request.js
@@ -0,0 +1,43 @@
+'use strict'
+
+function request(method, url, options) {
+  if (typeof url !== 'string') [method, url, options] = ['GET', method, url]
+  if (!options) options = {}
+
+  const xhr = new XMLHttpRequest
+  if (options.as === 'xml') {
+    options.as = 'document'
+    if (!options.type) options.type = 'text/xml'
+  }
+  if (options.as) xhr.responseType = options.as
+  if (options.type) xhr.overrideMimeType(options.type)
+  let hash = ''
+  const i = url.indexOf('#')
+  if (i !== -1) {
+    hash = url.slice(i + 1)
+    url = url.slice(0, i)
+  }
+  if (options.query) {
+    url += (url.includes('?') ? '&' : '?') + Object.keys(options.query).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(options.query[k])}`).join('&')
+  }
+  if (hash) url += '#' + hash
+  xhr.open(method, url, true)
+  xhr.send()
+
+  return new Promise((resolve, reject) => {
+    xhr.onload = () => {
+      if (xhr.status === 200) resolve(xhr.response)
+      else reject(new Error(`${method} ${url} failed: HTTP ${xhr.status} ${xhr.statusText}`))
+    }
+    xhr.onerror = () => reject(new Error(`${method} ${url} failed`))
+  })
+}
+
+request.get = function get(url, options) {
+  return request('GET', url, options)
+}
+request.post = function post(url, options) {
+  return request('POST', url, options)
+}
+
+module.exports = request
