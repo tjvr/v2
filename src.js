0b43560bcb72d91474b3254ca99a98d86a34828d
Nathan <nathan@users.noreply.github.com>
2017-03-16 19:04:16 -0400

    Move node to separate file

--- src.js
+++ src.js
@@ -242,31 +242,6 @@ class App extends View {
   }
 }
 
-const CyclicNode = require('./model/cyclic-node')
-class Node extends CyclicNode {
-  constructor(data, children) {
-    super(data, children)
-    this.parent = null
-    for (const c of this.children) c._moveTo(this)
-  }
-
-  _moveTo(parent) {
-    this.remove()
-    this.parent = parent
-  }
-  _removed() {
-    this.parent = null
-  }
-  remove() {
-    const parent = this.parent
-    if (!parent) return
-    const i = parent.children.indexOf(this)
-    if (i !== -1) parent.removeAt(i)
-  }
-
-  *parents() {for (let p = this; p; p = p.parent) yield p}
-}
-
-Object.assign(v2, {App, Node})
+Object.assign(v2, {App})
 
 module.exports = v2
